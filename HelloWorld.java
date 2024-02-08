0df6cafa49f7a9921a5ab057d79d04164e606ccb
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-02-08 22:29:44 +0000

    Used linter

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,10 @@
+// Copyright (c) 2024 Chris Di Bert All rights reserved.
+// .
+// Created by: Christopher Di Bert
+// Date : Feb.7, 2024
+// This program prints hello to my friend and prints
+// a face
+
 package com.example;
 
 public class HelloWorld {
