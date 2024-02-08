0d37c37757590097d9cffb8c2405ba4219e828fd
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-02-08 19:14:01 +0000

    lint test 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class HelloWorld {
 
+  // Linter being tested
+
   public static void main(final String[] args) {
     System.out.println("Hello, Spencer!");
     System.out.println("Look at this funny face: ( ͡° ʖ ͡°)");
