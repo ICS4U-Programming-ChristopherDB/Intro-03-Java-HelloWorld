0c199a8f70b59eb03803d4b41633c32134b077d5
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-02-08 21:24:01 +0000

    testing linter

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
   // Linter being tested
 
-  public static void main(final String[] args) {
+  public static void main(String[] args) {
     System.out.println("Hello, Spencer!");
     System.out.println("Look at this funny face: ( ͡° ʖ ͡°)");
   }
